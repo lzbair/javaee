84ca7e23c8907e72c56572f2d874227811efc9f2
Lahcen Zbair <l.zbair@yahoo.fr>
2020-10-12 16:47:51 +0100

    Use spring jdbc template

--- db-connectivity/src/main/java/org/tutoring/db/connectivity/BookMapper.java
+++ db-connectivity/src/main/java/org/tutoring/db/connectivity/BookMapper.java
@@ -1,11 +1,18 @@
 package org.tutoring.db.connectivity;
 
+import org.springframework.jdbc.core.RowMapper;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class BookMapper implements ResultMapper{
+public class BookMapper implements ResultMapper, RowMapper<Book> {
     @Override
     public Book newItem(ResultSet rs) throws SQLException {
+        return new Book(rs.getString("Title"));
+    }
+
+    @Override
+    public Book mapRow(ResultSet rs, int rowNum) throws SQLException {
         return new Book(rs.getString("Title"));
     }
 }
--- db-connectivity/src/main/java/org/tutoring/db/connectivity/DBClient.java
+++ db-connectivity/src/main/java/org/tutoring/db/connectivity/DBClient.java
@@ -1,5 +1,7 @@
 package org.tutoring.db.connectivity;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
 import javax.sql.DataSource;
 
 public class DBClient {
@@ -7,6 +9,11 @@ public class DBClient {
 
     public static void main(String[] args) throws Exception {
         DataSource ds = JNDI.lookup(JNDI.TUTORING_DS_NAME);
+        standard(ds);
+        spring(ds);
+    }
+
+    private static void standard(DataSource ds) throws java.sql.SQLException {
         Database db = new Database(ds);
         String bookTable = "CREATE TABLE Books (" +
                 "BookID int, " +
@@ -23,5 +30,12 @@ public class DBClient {
         //execute once : db.execute(new Batch(insertBooks));
 
         db.execute(new Select("SELECT * from Books", new BookMapper())).stream().forEach(System.out::println);
+    }
+
+
+    private static void spring(DataSource ds) {
+        final JdbcTemplate jdbcTemplate = new JdbcTemplate(ds);
+        String query = "SELECT * from Books";
+        jdbcTemplate.query(query, new BookMapper()).stream().forEach(System.out::println);
     }
 }
